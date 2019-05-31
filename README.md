288f667690ad203572f47959fefdcea0dc124794
Sherry Freitas <sherry.freitas@gmail.com>
2019-05-31 12:15:01 -0700

    adds initial files

---
+++ README.md
@@ -0,0 +1,22 @@
+# NODE101 Express Server
+
+## To install:
+```
+npm install
+```
+---
+## To run:
+```
+npm start
+```
+Then go to http://localhost:3000/ and http://localhost:3000/data in your browser
+---
+## To test:
+Start the server:
+```
+npm start
+```
+Run the tests in another terminal:
+```
+npm test
+```
--- server/app.js
+++ server/app.js
@@ -1,10 +1,24 @@
 // import files and packages up here
-
+const express = require('express');
+const morgan = require('morgan');
+const data = require('./data.json');
 
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+app.use(morgan('dev')); //logs all requests to STDOUT
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type','text/html');
+    res.status(200).end();
+});
+
+app.get('/data', function(req, res) {
+    res.setHeader('Content-Type','application/json');
+    res.send(data);
+    res.status(200).end();
+});
 
 // finally export the express application
 module.exports = app;
